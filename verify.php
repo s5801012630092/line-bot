3ec3b6f54d6f74623dcb94648d262cec0bccd3c8
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-09 22:33:52 +0700

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$access_token = 'jlMoaDCsiy7kbz0JIpj/aFDVojwOc6V+jHZweM8HW4eaFzWVKTUF0UyNToc8RYq93NII5Nx4CpoD1KdlI0Pjld0HOXLOSzH16/DU0ocf9YNr8nRRt1CL8dClQqmQb98sdSdjOulzdZ+UGQfYfNgn0wdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
