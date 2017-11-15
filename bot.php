05c92cc8855c8d7f19d9a5652d67f77bdf7fd7cb
s5801012630092 <31880220+s5801012630092@users.noreply.github.com>
2017-11-15 18:30:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -190,7 +190,7 @@ if (!is_null($events['events'])) {
 					$number = array();
 					$address = array();
 					$urll = array();
-					for ($x = 0; $x <ช 5; $x++) {
+					for ($x = 0; $x <= 5; $x++) {
 						$mes = $obj['results'][$x]['place_id']; 
 						$url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=$mes&key=AIzaSyBEA0UcZj9m-fYvwGTx0aoITGJxyWLdGm4";
 						$curl_handle = curl_init();
